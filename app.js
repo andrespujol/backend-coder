e750d138a9c29d7141d3a6e5cc2f0476b459e84d
Andrés Pujol <andrespujol@gmail.com>
2022-09-24 17:27:03 -0300

    manejo de archivos

--- app.js
+++ app.js
@@ -1,42 +1,104 @@
-class Usuario {
-    constructor (nombre, apellido, libros = [], mascotas = []) {
-        this.nombre = nombre,
-        this.apellido = apellido,
-        this.libros = libros,
-        this.mascotas = mascotas
+const fs = require ('fs');
+
+
+class Contenedor {
+
+    constructor(file) {
+        this.file = file
     }
-    getFullName(){
-        return `${this.nombre} ${this.apellido}`
+    async read() {
+        try {
+            let data = await fs.promises.readFile("./" + this.file, "utf-8")
+            return data
+
+        } catch (error) {
+            throw Error("Error al leer el archivo")
+        }
     }
-    addMascota(string){
-        this.mascotas.push(string)
+    async write(objeto, msg) {
+        try {
+            await fs.promises.writeFile("./" + this.file, JSON.stringify(objeto, null, 2))
+            console.log(msg)
+        } catch (error) {
+            throw Error("Error al escribir en el archivo")
+        }
     }
-    countMascotas(){
-        return this.mascotas.length
+    async save(producto) {
+        let newId = 1
+        let objeto = {}
+
+        let data = await this.read()
+        let archivo = JSON.parse(data)
+
+        if(!data) {
+            producto.id = newId
+            objeto = [producto]
+        } else {
+            producto.id = archivo[archivo.length - 1].id + 1
+            objeto = producto
+        }
+        archivo.push(objeto)
+
+        await this.write(archivo, "Agregado!")
     }
-    addBook(libro) {
-        this.libros.push(libro)
+    async getById(num) {
+        let data = await this.read()
+        let object = JSON.parse(data)
+
+        let result = object.filter( producto => producto.id == num)
+        return result
     }
-    getBookNames(){
-        return this.libros.map(libro=>libro.nombre)
+
+    async getAll() {
+        let data = await this.read()
+        let object = JSON.parse(data)
+
+        return object
     }
+
+    async deleteById(num) {
+        let data = await this.read()
+        let object = JSON.parse(data)
+
+        let producto = object.find( producto => producto.id == num)
+
+        if(producto) {
+            let index = object.indexOf(producto)
+            console.log(index)
+            object.splice(index, 1)
+            await this.write(object, `producto con ID: ${num} eliminado`)
+        } else {
+            console.log(`no existe el producto con ID: ${num} `)
+        }
+    }
+
+    async deleteAll() {
+        let array = []
+        await this.write(array, "Se eliminaron todos los productos")
+    }
+
 }
 
-let nuevoUsuario = new Usuario ('Andrés', 'Pujol')
+let contenedor = new Contenedor("productos.json")
 
-nuevoUsuario.addMascota("Bernie")
-nuevoUsuario.addMascota("Negro")
-nuevoUsuario.addMascota("Firu")
+//Ejemplo para verificar
+async function consultarDesafio() {
+    const newProduct = {
+        id: 5,
+        title: "Pantalón",
+        price: 2500,
+        stock: 5,
+        pictureUrl: "https://i.ibb.co/qp2R7wn/4.jpg",
+        category: "Pantalón",
+        description: "Pantalón negro"
+    }
 
-nuevoUsuario.addBook({
-    nombre: "Harry Potter",
-    autor: "J. K. Rowling"
-})
-nuevoUsuario.addBook({
-    nombre: "El señor de los anillos",
-    autor: "J. R. R. Tolkien"
-})
+    await contenedor.save(newProduct)
 
-console.log(nuevoUsuario.getFullName())
-console.log(nuevoUsuario.countMascotas())
-console.log(nuevoUsuario.getBookNames())
+    console.log(await contenedor.getById(2))
+
+    console.log(await contenedor.getAll())
+
+    await contenedor.deleteById(5)
+}
+consultarDesafio() 
